df617e43031c274e4a1315165ca57a98eb87d489
kbailov@gmail.com <kbailov@gmail.com@89a3928e-a5c4-6741-2893-80d793832b1f>
2013-01-17 19:59:33 +0000

    #252/#227 update return values for the services to distinguish between them by return value

--- samples/SampleWebServices/TestServices/src/bg/unisofia/fmi/ws/addition/AdditionServiceStandard.java
+++ samples/SampleWebServices/TestServices/src/bg/unisofia/fmi/ws/addition/AdditionServiceStandard.java
@@ -5,7 +5,7 @@ public class AdditionServiceStandard implements IAdditionService {
 	@Override
 	public double add(double a, double b) {
 		double result = a + b;
-		return result;
+		return 333;
 	}
 
 }
