85abac4a96822735b8d2f568f92d364f4dfd86f2
kbailov@gmail.com <kbailov@gmail.com@89a3928e-a5c4-6741-2893-80d793832b1f>
2013-01-17 19:54:27 +0000

    #261/#262 Create a maven module for the BPEL extension performing dynamic web service composition

---
+++ trunk/dwsc-master/yani-bpel-extension/src/main/java/bg/unisofia/fmi/dwsc/bpel/extension/App.java
@@ -0,0 +1,13 @@
+package bg.unisofia.fmi.dwsc.bpel.extension;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
