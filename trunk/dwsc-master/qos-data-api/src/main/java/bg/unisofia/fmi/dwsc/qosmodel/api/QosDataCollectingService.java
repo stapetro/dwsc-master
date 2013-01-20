625bfef57482a783690c047f48783b67d61c6050
stanislav.i.petrov@gmail.com <stanislav.i.petrov@gmail.com@89a3928e-a5c4-6741-2893-80d793832b1f>
2013-01-20 20:01:44 +0000

    #263/#264 "Build Service QoS monitoring module" / "Create Axis2 QoS monitor module"
    Initial creation and deployment of QoSMontirongModule.

---
+++ trunk/dwsc-master/qos-data-api/src/main/java/bg/unisofia/fmi/dwsc/qosmodel/api/QosDataCollectingService.java
@@ -0,0 +1,19 @@
+package bg.unisofia.fmi.dwsc.qosmodel.api;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+import bg.unisofia.fmi.dwsc.qosmodel.api.exceptions.PublishServiceException;
+
+/**
+ * Represents service for collecting QoS raw data from monitoring module.
+ * 
+ */
+@WebService
+@SOAPBinding(style = Style.RPC)
+public interface QosDataCollectingService {
+
+
+}
