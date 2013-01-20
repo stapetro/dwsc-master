274e3f663fbb6477347d0b6cb7336674e1b75236
stanislav.i.petrov@gmail.com <stanislav.i.petrov@gmail.com@89a3928e-a5c4-6741-2893-80d793832b1f>
2013-01-20 20:29:22 +0000

    #263/#264 "Build Service QoS monitoring module" / "Create Axis2 QoS monitor module"
    Calculating SOAP message size.

--- trunk/dwsc-master/qosmonitor/src/main/java/bg/unisofia/fmi/dwsc/qosmonitoring/QosMonitoringHandler.java
+++ trunk/dwsc-master/qosmonitor/src/main/java/bg/unisofia/fmi/dwsc/qosmonitoring/QosMonitoringHandler.java
@@ -1,7 +1,14 @@
 package bg.unisofia.fmi.dwsc.qosmonitoring;
 
-import javax.xml.stream.XMLStreamException;
+import java.io.ByteArrayOutputStream;
+
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stax.StAXSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.apache.axiom.soap.SOAPEnvelope;
 import org.apache.axis2.AxisFault;
@@ -10,7 +17,6 @@ import org.apache.axis2.context.OperationContext;
 import org.apache.axis2.context.ServiceContext;
 import org.apache.axis2.handlers.AbstractHandler;
 import org.apache.log4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class QosMonitoringHandler extends AbstractHandler {
 
@@ -26,23 +32,25 @@ public class QosMonitoringHandler extends AbstractHandler {
 			throws AxisFault {
 		SOAPEnvelope envelope = msgContext.getEnvelope();
 		XMLStreamReader xmlReader = envelope.getXMLStreamReader();
+		TransformerFactory tf = TransformerFactory.newInstance();
 		int bytes = 0;
 		try {
-			while (xmlReader.hasNext()) {
-				try {
-					bytes += xmlReader.getTextLength();
-				} catch(IllegalStateException ex) {
-					
-				}
-				xmlReader.next();
+			Transformer t = tf.newTransformer();
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			StreamResult sResult = new StreamResult(outputStream);
+			StAXSource staxSource = new StAXSource(xmlReader);
+			try {
+				t.transform(staxSource, sResult);
+				bytes = outputStream.size();
+			} catch (TransformerException e) {
+				logger.error("Error transforming", e);
 			}
-		} catch (XMLStreamException e) {
-			logger.error("Error reading SOAP msg", e);
-			throw new AxisFault("Error reading SOAP msg");
+		} catch (TransformerConfigurationException e1) {
+			logger.error("Error transforming", e1);
 		}
 		ServiceContext srvContext = msgContext.getServiceContext();
 		OperationContext opContext = msgContext.getOperationContext();
-		logger.info("Soap msg size '" + bytes + "' for srv '"
+		logger.info("Soap msg size '" + bytes + "' bytes for srv '"
 				+ srvContext.getName() + "' opName '"
 				+ opContext.getOperationName() + "'");
 		return InvocationResponse.CONTINUE;
