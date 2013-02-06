0884d2e9e573871be98ab0c01b2ffcdd28a53b33
kbailov@gmail.com <kbailov@gmail.com@89a3928e-a5c4-6741-2893-80d793832b1f>
2013-02-06 18:36:02 +0000

    #261/#271 Update BPEL QoS Parser to read Qos Cost data

--- dwsc-master/yani-bpel-extension/src/test/java/bg/unisofia/fmi/dwsc/bpel/extension/util/QoSUtilTest.java
+++ dwsc-master/yani-bpel-extension/src/test/java/bg/unisofia/fmi/dwsc/bpel/extension/util/QoSUtilTest.java
@@ -18,7 +18,15 @@ public class QoSUtilTest {
 	private static final String QOS_ELEMENT_AS_STRING = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 			+ "<yani:ComposeQoS odebpelc:lineno=\"63\" xmlns:odebpelc=\"urn:org.apache.ode.bpel.compiler\" xmlns:yani=\"http://yaniwscomposer.com/YaniWsComposer\">"
 			+ "	<yani:QoS odebpelc:lineno=\"64\">"
-			+ "		<yani:qualityAttribute name=\"executionTime\" odebpelc:lineno=\"65\" value=\"0.432\"/>"
+			+ "		<yani:qualityAttribute name=\"executionTime\" odebpelc:lineno=\"65\" value=\"0.432\" cost=\"0.3\"/>"
+			+ "		<yani:qualityAttribute name=\"availability\" odebpelc:lineno=\"66\" value=\"0.7\" cost=\"0.5\"/>"
+			+ "		<yani:qualityAttribute name=\"throughput\" odebpelc:lineno=\"67\" value=\"33\" cost=\"0.2\"/>"
+			+ "	</yani:QoS>" + "</yani:ComposeQoS>";
+	
+	private static final String QOS_ELEMENT_WITH_MISSING_COST_AS_STRING = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+			+ "<yani:ComposeQoS odebpelc:lineno=\"63\" xmlns:odebpelc=\"urn:org.apache.ode.bpel.compiler\" xmlns:yani=\"http://yaniwscomposer.com/YaniWsComposer\">"
+			+ "	<yani:QoS odebpelc:lineno=\"64\">"
+			+ "		<yani:qualityAttribute name=\"executionTime\" odebpelc:lineno=\"65\" value=\"0.432\" cost=\"0.3\"/>"
 			+ "		<yani:qualityAttribute name=\"availability\" odebpelc:lineno=\"66\" value=\"0.7\"/>"
 			+ "		<yani:qualityAttribute name=\"throughput\" odebpelc:lineno=\"67\" value=\"33\"/>"
 			+ "	</yani:QoS>" + "</yani:ComposeQoS>";
@@ -59,6 +67,52 @@ public class QoSUtilTest {
 		Assert.assertEquals("0.432", executionTimeValue);
 		Assert.assertEquals("0.7", availabilityValue);
 		Assert.assertEquals("33", throughputValue);
+	}
+	
+	@Test
+	public void testQoSUtilsWithValidElementForCorrectCostWithMissingCosts()
+			throws SAXException, IOException {
+		QoSUtils qosUtil = new QoSUtils();
+		Element qosElement = DOMUtils.stringToDOM(QOS_ELEMENT_WITH_MISSING_COST_AS_STRING);
+		QualityProfile resultQualityProfile = qosUtil
+				.getQualityProfile(qosElement);
+
+		Map<QualityAttributeEnum, String> qualityCostsMap = resultQualityProfile
+				.getQualityCost();
+
+		String executionTimeValue = qualityCostsMap
+				.get(QualityAttributeEnum.EXECUTION_TIME);
+		String availabilityValue = qualityCostsMap
+				.get(QualityAttributeEnum.AVAILABILITY);
+		String throughputValue = qualityCostsMap
+				.get(QualityAttributeEnum.THROUGHPUT);
+
+		Assert.assertEquals("0.3", executionTimeValue);
+		Assert.assertEquals("0", availabilityValue);
+		Assert.assertEquals("0", throughputValue);
+	}
+	
+	@Test
+	public void testQoSUtilsWithValidElementForCorrectQoSCost()
+			throws SAXException, IOException {
+		QoSUtils qosUtil = new QoSUtils();
+		Element qosElement = DOMUtils.stringToDOM(QOS_ELEMENT_AS_STRING);
+		QualityProfile resultQualityProfile = qosUtil
+				.getQualityProfile(qosElement);
+
+		Map<QualityAttributeEnum, String> qualityCostsMap = resultQualityProfile
+				.getQualityCost();
+
+		String executionTimeValue = qualityCostsMap
+				.get(QualityAttributeEnum.EXECUTION_TIME);
+		String availabilityValue = qualityCostsMap
+				.get(QualityAttributeEnum.AVAILABILITY);
+		String throughputValue = qualityCostsMap
+				.get(QualityAttributeEnum.THROUGHPUT);
+
+		Assert.assertEquals("0.3", executionTimeValue);
+		Assert.assertEquals("0.5", availabilityValue);
+		Assert.assertEquals("0.2", throughputValue);
 	}
 
 	@Test
--- dwsc-master/yani-model/src/main/java/bg/unisofia/fmi/dwsc/yani/model/QualityProfile.java
+++ dwsc-master/yani-model/src/main/java/bg/unisofia/fmi/dwsc/yani/model/QualityProfile.java
@@ -2,11 +2,21 @@ package bg.unisofia.fmi.dwsc.yani.model;
 
 import java.util.Map;
 
+/**
+ * Class representing quality profile for a BPEL Process
+ * 
+ * @author krasimir.baylov
+ * 
+ */
 public class QualityProfile {
 	private Map<QualityAttributeEnum, String> qualityRequirements;
+	private Map<QualityAttributeEnum, String> qualityCost;
 
-	public QualityProfile(Map<QualityAttributeEnum, String> qualityRequirements) {
+	public QualityProfile(
+			Map<QualityAttributeEnum, String> qualityRequirements,
+			Map<QualityAttributeEnum, String> qualityCost) {
 		this.qualityRequirements = qualityRequirements;
+		this.qualityCost = qualityCost;
 	}
 
 	public Map<QualityAttributeEnum, String> getQualityRequirements() {
@@ -16,6 +26,14 @@ public class QualityProfile {
 	public void setQualityRequirements(
 			Map<QualityAttributeEnum, String> qualityRequirements) {
 		this.qualityRequirements = qualityRequirements;
+	}
+
+	public Map<QualityAttributeEnum, String> getQualityCost() {
+		return qualityCost;
+	}
+
+	public void setQualityCost(Map<QualityAttributeEnum, String> qualityCost) {
+		this.qualityCost = qualityCost;
 	}
 
 }
