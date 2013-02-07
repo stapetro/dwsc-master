3388dc87d42c0ba2e28c42c33df15ede3f496259
kbailov@gmail.com <kbailov@gmail.com@89a3928e-a5c4-6741-2893-80d793832b1f>
2013-02-07 14:18:52 +0000

    #261/#273 Implement help functionality for creating service combinations

--- trunk/dwsc-master/BpelModelAvaluator/src/main/java/bg/unisofia/fmi/dwsc/yani/evaluator/service/ServiceSelector.java
+++ trunk/dwsc-master/BpelModelAvaluator/src/main/java/bg/unisofia/fmi/dwsc/yani/evaluator/service/ServiceSelector.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Map;
 
 import bg.unisofia.fmi.dwsc.yani.model.PartnerLinkDefinition;
-import bg.unisofia.fmi.dwsc.yani.model.QualityAttributeEnum;
 import bg.unisofia.fmi.dwsc.yani.model.WebService;
+import bg.unisofia.fmi.dwsc.yani.model.qos.IQualityAttribute;
+import bg.unisofia.fmi.dwsc.yani.model.qos.QualityAttributeEnum;
+import bg.unisofia.fmi.dwsc.yani.model.qos.utils.QualityAttributeFactory;
 
 public class ServiceSelector {
 
@@ -38,26 +40,59 @@ public class ServiceSelector {
 		List<WebService> wsList = new LinkedList<WebService>();
 		String category = "addition";
 
-		Map<QualityAttributeEnum, String> qosStandard = new HashMap<QualityAttributeEnum, String>();
-		qosStandard.put(QualityAttributeEnum.EXECUTION_TIME, "0,1");
-		qosStandard.put(QualityAttributeEnum.AVAILABILITY, "1");
-		qosStandard.put(QualityAttributeEnum.THROUGHPUT, "120");
-		Map<QualityAttributeEnum, String> qosMinStandard = new HashMap<QualityAttributeEnum, String>();
-		qosMinStandard.put(QualityAttributeEnum.EXECUTION_TIME, "0,5");
-		qosMinStandard.put(QualityAttributeEnum.AVAILABILITY, "0.5");
-		qosMinStandard.put(QualityAttributeEnum.THROUGHPUT, "44");
+		Map<QualityAttributeEnum, IQualityAttribute> qosStandard = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosStandard.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "0.2"));
+
+		qosStandard.put(QualityAttributeEnum.AVAILABILITY,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.AVAILABILITY, "1"));
+
+		qosStandard.put(QualityAttributeEnum.THROUGHPUT,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.THROUGHPUT, "120"));
+
+		Map<QualityAttributeEnum, IQualityAttribute> qosMinStandard = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosMinStandard.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "0.5"));
+
+		qosMinStandard.put(QualityAttributeEnum.AVAILABILITY,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.AVAILABILITY, "0.5"));
+
+		qosMinStandard.put(QualityAttributeEnum.THROUGHPUT,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.THROUGHPUT, "44"));
+
 		WebService wsStandard = new WebService(
 				"http://localhost:8080/TestServices/services/AdditionServiceStandard.AdditionServiceStandardHttpSoap12Endpoint/",
 				category, qosStandard, qosMinStandard);
 
-		Map<QualityAttributeEnum, String> qosSlow = new HashMap<QualityAttributeEnum, String>();
-		qosSlow.put(QualityAttributeEnum.EXECUTION_TIME, "0,9");
-		qosSlow.put(QualityAttributeEnum.AVAILABILITY, "1");
-		qosSlow.put(QualityAttributeEnum.THROUGHPUT, "20");
-		Map<QualityAttributeEnum, String> qosMinSlow = new HashMap<QualityAttributeEnum, String>();
-		qosMinSlow.put(QualityAttributeEnum.EXECUTION_TIME, "1,5");
-		qosMinSlow.put(QualityAttributeEnum.AVAILABILITY, "0.5");
-		qosMinSlow.put(QualityAttributeEnum.THROUGHPUT, "22");
+		Map<QualityAttributeEnum, IQualityAttribute> qosSlow = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosSlow.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "0.9"));
+
+		qosSlow.put(QualityAttributeEnum.AVAILABILITY, QualityAttributeFactory
+				.getQualityAttribute(QualityAttributeEnum.AVAILABILITY, "1"));
+
+		qosSlow.put(QualityAttributeEnum.THROUGHPUT, QualityAttributeFactory
+				.getQualityAttribute(QualityAttributeEnum.THROUGHPUT, "20"));
+
+		Map<QualityAttributeEnum, IQualityAttribute> qosMinSlow = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosMinSlow.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "1.5"));
+
+		qosMinSlow.put(QualityAttributeEnum.AVAILABILITY,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.AVAILABILITY, "0.5"));
+
+		qosMinSlow.put(QualityAttributeEnum.THROUGHPUT, QualityAttributeFactory
+				.getQualityAttribute(QualityAttributeEnum.THROUGHPUT, "22"));
+
 		WebService wsSlow = new WebService(
 				"http://localhost:8080/TestServices/services/AdditionServiceSlow.AdditionServiceSlowdHttpSoap12Endpoint/",
 				category, qosSlow, qosMinSlow);
@@ -72,26 +107,59 @@ public class ServiceSelector {
 		List<WebService> wsList = new LinkedList<WebService>();
 		String category = "pow";
 
-		Map<QualityAttributeEnum, String> qosStandard = new HashMap<QualityAttributeEnum, String>();
-		qosStandard.put(QualityAttributeEnum.EXECUTION_TIME, "0,1");
-		qosStandard.put(QualityAttributeEnum.AVAILABILITY, "1");
-		qosStandard.put(QualityAttributeEnum.THROUGHPUT, "120");
-		Map<QualityAttributeEnum, String> qosMinStandard = new HashMap<QualityAttributeEnum, String>();
-		qosMinStandard.put(QualityAttributeEnum.EXECUTION_TIME, "0,5");
-		qosMinStandard.put(QualityAttributeEnum.AVAILABILITY, "0.5");
-		qosMinStandard.put(QualityAttributeEnum.THROUGHPUT, "44");
+		Map<QualityAttributeEnum, IQualityAttribute> qosStandard = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosStandard.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "0.1"));
+
+		qosStandard.put(QualityAttributeEnum.AVAILABILITY,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.AVAILABILITY, "1"));
+
+		qosStandard.put(QualityAttributeEnum.THROUGHPUT,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.THROUGHPUT, "120"));
+
+		Map<QualityAttributeEnum, IQualityAttribute> qosMinStandard = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosMinStandard.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "0.5"));
+
+		qosMinStandard.put(QualityAttributeEnum.AVAILABILITY,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.AVAILABILITY, "0.5"));
+
+		qosMinStandard.put(QualityAttributeEnum.THROUGHPUT,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.THROUGHPUT, "44"));
+
 		WebService wsStandard = new WebService(
 				"http://localhost:8080/TestServices/services/PowServiceStandard.PowServiceStandardHttpSoap12Endpoint/",
 				category, qosStandard, qosMinStandard);
 
-		Map<QualityAttributeEnum, String> qosSlow = new HashMap<QualityAttributeEnum, String>();
-		qosSlow.put(QualityAttributeEnum.EXECUTION_TIME, "0,9");
-		qosSlow.put(QualityAttributeEnum.AVAILABILITY, "1");
-		qosSlow.put(QualityAttributeEnum.THROUGHPUT, "20");
-		Map<QualityAttributeEnum, String> qosMinSlow = new HashMap<QualityAttributeEnum, String>();
-		qosMinSlow.put(QualityAttributeEnum.EXECUTION_TIME, "1,5");
-		qosMinSlow.put(QualityAttributeEnum.AVAILABILITY, "0.5");
-		qosMinSlow.put(QualityAttributeEnum.THROUGHPUT, "22");
+		Map<QualityAttributeEnum, IQualityAttribute> qosSlow = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosSlow.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "0.9"));
+
+		qosSlow.put(QualityAttributeEnum.AVAILABILITY, QualityAttributeFactory
+				.getQualityAttribute(QualityAttributeEnum.AVAILABILITY, "1"));
+
+		qosSlow.put(QualityAttributeEnum.THROUGHPUT, QualityAttributeFactory
+				.getQualityAttribute(QualityAttributeEnum.THROUGHPUT, "20"));
+
+		Map<QualityAttributeEnum, IQualityAttribute> qosMinSlow = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosMinSlow.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "1.5"));
+
+		qosMinSlow.put(QualityAttributeEnum.AVAILABILITY,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.AVAILABILITY, "0.5"));
+
+		qosMinSlow.put(QualityAttributeEnum.THROUGHPUT, QualityAttributeFactory
+				.getQualityAttribute(QualityAttributeEnum.THROUGHPUT, "20"));
+
 		WebService wsSlow = new WebService(
 				"http://localhost:8080/TestServices/services/PowServiceSlow.PowServiceSlowHttpSoap12Endpoint/",
 				category, qosSlow, qosMinSlow);
---
+++ trunk/dwsc-master/BpelModelAvaluator/src/test/java/bg/unisofia/fmi/dwsc/yani/evaluator/service/ServiceCompositionAnalyserTest.java
@@ -0,0 +1,134 @@
+package bg.unisofia.fmi.dwsc.yani.evaluator.service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import bg.unisofia.fmi.dwsc.yani.evaluator.ServiceCompositionAnalyser;
+import bg.unisofia.fmi.dwsc.yani.model.PartnerLinkDefinition;
+import bg.unisofia.fmi.dwsc.yani.model.WebService;
+
+public class ServiceCompositionAnalyserTest {
+
+	// private static final String EXTECTED_ENDPOINTS_IN_GENERATED_MARIX = new
+	// String{{"additionEndpoint1", "additionEndpoint1"}}
+
+	@Test
+	public void testGetMatrixSize() {
+		ServiceCompositionAnalyser analyser = new ServiceCompositionAnalyser();
+		List<PartnerLinkDefinition> plDefinitionList = getPlDefinitionList();
+
+		int[] matrixSize = analyser.getMatrixSize(plDefinitionList);
+
+		Assert.assertEquals(2, matrixSize.length);
+		Assert.assertEquals(4, matrixSize[0]);
+		Assert.assertEquals(2, matrixSize[1]);
+
+	}
+
+	@Test
+	public void testUpdateMatrixFirstColumnRepeatInterval6() {
+		WebService ws1 = new WebService("endpoint1", "addition", null, null);
+		WebService ws2 = new WebService("endpoint2", "addition", null, null);
+		List<WebService> wsList = new LinkedList<WebService>();
+		wsList.add(ws1);
+		wsList.add(ws2);
+
+		int columnNumber = 0;
+		int repeatInterval = 6;
+
+		WebService[][] wsMatrix = new WebService[12][3];
+		ServiceCompositionAnalyser analyser = new ServiceCompositionAnalyser();
+		analyser.updateMatrix(wsList, wsMatrix, columnNumber, repeatInterval);
+
+		for (int i = 0; i < 6; i++) {
+			String endpoint = wsMatrix[i][columnNumber].getEndPoint();
+			Assert.assertEquals("endpoint1", endpoint);
+		}
+
+		for (int i = 6; i < 11; i++) {
+			String endpoint = wsMatrix[i][columnNumber].getEndPoint();
+			Assert.assertEquals("endpoint2", endpoint);
+		}
+	}
+
+	@Test
+	public void testGetServiceCompositionMatrixSize() {
+		List<PartnerLinkDefinition> plDefinitionList = getPlDefinitionList();
+		ServiceCompositionAnalyser analyser = new ServiceCompositionAnalyser();
+		WebService[][] webServiceCompositionMatrix = analyser
+				.getServiceCompositionMatrix(plDefinitionList);
+
+		Assert.assertEquals(webServiceCompositionMatrix.length, 4);
+		Assert.assertEquals(webServiceCompositionMatrix[0].length, 2);
+
+		// for (int i = 0; i < webServiceCompositionMatrix.length; i++) {
+		// for (int j = 0; j < webServiceCompositionMatrix[i].length; j++) {
+		// System.out.print(webServiceCompositionMatrix[i][j]
+		// .getEndPoint());
+		// System.out.print("\t");
+		// }
+		// System.out.println();
+		// }
+	}
+
+	@Test
+	public void testGetServiceCompositionMatrixContentByEndpoints() {
+		List<PartnerLinkDefinition> plDefinitionList = getPlDefinitionList();
+		ServiceCompositionAnalyser analyser = new ServiceCompositionAnalyser();
+		WebService[][] webServiceCompositionMatrix = analyser
+				.getServiceCompositionMatrix(plDefinitionList);
+
+		Assert.assertEquals("additionEndpoint1",
+				webServiceCompositionMatrix[0][0].getEndPoint());
+		Assert.assertEquals("powEndpoint1",
+				webServiceCompositionMatrix[0][1].getEndPoint());
+		Assert.assertEquals("additionEndpoint1",
+				webServiceCompositionMatrix[1][0].getEndPoint());
+		Assert.assertEquals("powEndpoint2",
+				webServiceCompositionMatrix[1][1].getEndPoint());
+		Assert.assertEquals("additionEndpoint2",
+				webServiceCompositionMatrix[2][0].getEndPoint());
+		Assert.assertEquals("powEndpoint1",
+				webServiceCompositionMatrix[2][1].getEndPoint());
+		Assert.assertEquals("additionEndpoint2",
+				webServiceCompositionMatrix[3][0].getEndPoint());
+		Assert.assertEquals("powEndpoint2",
+				webServiceCompositionMatrix[3][1].getEndPoint());
+	}
+
+	private List<PartnerLinkDefinition> getPlDefinitionList() {
+		List<PartnerLinkDefinition> plDefinitionList = new LinkedList<PartnerLinkDefinition>();
+
+		PartnerLinkDefinition plDefinition1 = new PartnerLinkDefinition(
+				"TestPL1", "addition", "endpoint1", null);
+
+		WebService wsAddition1 = new WebService("additionEndpoint1",
+				"addition", null, null);
+		WebService wsAddition2 = new WebService("additionEndpoint2",
+				"addition", null, null);
+		List<WebService> additionWsList = new LinkedList<WebService>();
+		additionWsList.add(wsAddition1);
+		additionWsList.add(wsAddition2);
+		plDefinition1.setAssiciatedServiceList(additionWsList);
+		plDefinitionList.add(plDefinition1);
+
+		// second partnerlink with services
+		PartnerLinkDefinition plDefinition2 = new PartnerLinkDefinition(
+				"TestPL2", "pow", "endpoint2", null);
+		WebService wsPow1 = new WebService("powEndpoint1", "addition", null,
+				null);
+		WebService wsPow2 = new WebService("powEndpoint2", "addition", null,
+				null);
+		List<WebService> powWsList = new LinkedList<WebService>();
+		powWsList.add(wsPow1);
+		powWsList.add(wsPow2);
+		plDefinition2.setAssiciatedServiceList(powWsList);
+		plDefinitionList.add(plDefinition2);
+
+		return plDefinitionList;
+	}
+}
