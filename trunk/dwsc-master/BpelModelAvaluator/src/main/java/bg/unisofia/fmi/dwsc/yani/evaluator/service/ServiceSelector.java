536432257865a3d1de1442ed38dee664a839aa1f
kbailov@gmail.com <kbailov@gmail.com@89a3928e-a5c4-6741-2893-80d793832b1f>
2013-02-21 19:21:56 +0000

    000/000 Add multiplicity mock for test purpose to prepare the demo for the master defence

--- trunk/dwsc-master/BpelModelAvaluator/src/main/java/bg/unisofia/fmi/dwsc/yani/evaluator/service/ServiceSelector.java
+++ trunk/dwsc-master/BpelModelAvaluator/src/main/java/bg/unisofia/fmi/dwsc/yani/evaluator/service/ServiceSelector.java
@@ -35,6 +35,8 @@ public class ServiceSelector {
 				wsList = getAdditionServices();
 			} else if (category.equals("pow")) {
 				wsList = getPowServices();
+			} else if (category.equals("multiplicity")){
+				wsList = getMultiplicityServices();
 			}
 			
 			//List<WebService> wsList = availableWebServices.get(category);
@@ -232,6 +234,98 @@ public class ServiceSelector {
 
 		WebService wsSlow = new WebService(
 				"http://localhost:8080/axis2-services/services/PowServiceSlow.PowServiceSlowHttpSoap12Endpoint/",
+				category, qosSlow, qosMinSlow, qosMaxSlow);
+
+		wsList.add(wsStandard);
+		wsList.add(wsSlow);
+
+		return wsList;
+	}
+	
+	private List<WebService> getMultiplicityServices() {
+		List<WebService> wsList = new LinkedList<WebService>();
+		String category = "multiplicity";
+
+		Map<QualityAttributeEnum, IQualityAttribute> qosStandard = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosStandard.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "0.1"));
+
+		qosStandard.put(QualityAttributeEnum.AVAILABILITY,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.AVAILABILITY, "1"));
+
+		qosStandard.put(QualityAttributeEnum.THROUGHPUT,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.THROUGHPUT, "120"));
+
+		Map<QualityAttributeEnum, IQualityAttribute> qosMinStandard = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosMinStandard.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "0.5"));
+
+		qosMinStandard.put(QualityAttributeEnum.AVAILABILITY,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.AVAILABILITY, "0.5"));
+
+		qosMinStandard.put(QualityAttributeEnum.THROUGHPUT,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.THROUGHPUT, "44"));
+
+		Map<QualityAttributeEnum, IQualityAttribute> qosMaxStandard = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosMaxStandard.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "0.05"));
+
+		qosMaxStandard.put(QualityAttributeEnum.AVAILABILITY,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.AVAILABILITY, "1"));
+
+		qosMaxStandard.put(QualityAttributeEnum.THROUGHPUT,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.THROUGHPUT, "190"));
+
+		WebService wsStandard = new WebService(
+				"http://localhost:8080/axis2-services/services/MultiplicityServiceStandard.MultiplicityServiceStandardHttpSoap12Endpoint/",
+				category, qosStandard, qosMinStandard, qosMaxStandard);
+
+		Map<QualityAttributeEnum, IQualityAttribute> qosSlow = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosSlow.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "0.9"));
+
+		qosSlow.put(QualityAttributeEnum.AVAILABILITY, QualityAttributeFactory
+				.getQualityAttribute(QualityAttributeEnum.AVAILABILITY, "0.9"));
+
+		qosSlow.put(QualityAttributeEnum.THROUGHPUT, QualityAttributeFactory
+				.getQualityAttribute(QualityAttributeEnum.THROUGHPUT, "40"));
+
+		Map<QualityAttributeEnum, IQualityAttribute> qosMinSlow = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosMinSlow.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "1.5"));
+
+		qosMinSlow.put(QualityAttributeEnum.AVAILABILITY,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.AVAILABILITY, "0.3"));
+
+		qosMinSlow.put(QualityAttributeEnum.THROUGHPUT, QualityAttributeFactory
+				.getQualityAttribute(QualityAttributeEnum.THROUGHPUT, "10"));
+
+		Map<QualityAttributeEnum, IQualityAttribute> qosMaxSlow = new HashMap<QualityAttributeEnum, IQualityAttribute>();
+		qosMaxSlow.put(QualityAttributeEnum.EXECUTION_TIME,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.EXECUTION_TIME, "0.5"));
+
+		qosMaxSlow.put(QualityAttributeEnum.AVAILABILITY,
+				QualityAttributeFactory.getQualityAttribute(
+						QualityAttributeEnum.AVAILABILITY, "1"));
+
+		qosMaxSlow.put(QualityAttributeEnum.THROUGHPUT, QualityAttributeFactory
+				.getQualityAttribute(QualityAttributeEnum.THROUGHPUT, "60"));
+
+		WebService wsSlow = new WebService(
+				"http://localhost:8080/axis2-services/services/MultiplicityServiceSlow.MultiplicityServiceSlowHttpSoap12Endpoint/",
 				category, qosSlow, qosMinSlow, qosMaxSlow);
 
 		wsList.add(wsStandard);
---
+++ trunk/dwsc-master/BpelModelAvaluator/src/test/java/bg/unisofia/fmi/dwsc/yani/evaluator/service/AllTests.java
@@ -0,0 +1,12 @@
+package bg.unisofia.fmi.dwsc.yani.evaluator.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CompositionAnalyserTest.class, QoSCalculatorTest.class,
+		ServiceCompositionUtilsTest.class })
+public class AllTests {
+
+}
