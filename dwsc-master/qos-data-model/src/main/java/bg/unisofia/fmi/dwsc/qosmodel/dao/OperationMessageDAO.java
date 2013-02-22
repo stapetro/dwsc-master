0116015eac9f87e0a415033e6b3bc4061de80f74
stanislav.i.petrov@gmail.com <stanislav.i.petrov@gmail.com@89a3928e-a5c4-6741-2893-80d793832b1f>
2013-02-22 21:26:26 +0000

    #000 Defect - JPA does NOT select all operation messages
    Fix is to directly retrieve all up-to-date operation messages.

--- dwsc-master/qos-data-model/src/main/java/bg/unisofia/fmi/dwsc/qosmodel/dao/OperationMessageDAO.java
+++ dwsc-master/qos-data-model/src/main/java/bg/unisofia/fmi/dwsc/qosmodel/dao/OperationMessageDAO.java
@@ -1,7 +1,12 @@
 package bg.unisofia.fmi.dwsc.qosmodel.dao;
 
-import javax.persistence.EntityManager;
+import java.util.Collection;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+import bg.unisofia.fmi.dwsc.qosmodel.domain.OperationInvocation;
 import bg.unisofia.fmi.dwsc.qosmodel.domain.OperationMessage;
 
 public class OperationMessageDAO extends
@@ -14,11 +19,25 @@ public class OperationMessageDAO extends
 	public OperationMessageDAO(EntityManager entityMgr) {
 		super(entityMgr);
 	}
-	
+
 	public OperationMessage save(OperationMessage opMessage) {
 		return this.save(opMessage, true);
 	}
-	
+
+	public Collection<OperationMessage> getOperationMessages(
+			String operationInvocationCorrelationId) {
+		if (operationInvocationCorrelationId == null
+				|| operationInvocationCorrelationId.equals("")) {
+			return null;
+		}
+		TypedQuery<OperationMessage> query = this.entityMgr
+				.createQuery(
+						"SELECT opMsg FROM OperationMessage opMsg WHERE opMsg.correlationId = :corrId",
+						OperationMessage.class);
+		query.setParameter("corrId", operationInvocationCorrelationId);
+		return query.getResultList();
+	}
+
 	/**
 	 * 
 	 * @param operationMessage
@@ -30,14 +49,11 @@ public class OperationMessageDAO extends
 	private OperationMessage save(OperationMessage operationMessage,
 			boolean createTransaction) {
 		if (operationMessage != null) {
-			OperationMessage foundOperationMsg = find(operationMessage
-					.getId());
+			OperationMessage foundOperationMsg = find(operationMessage.getId());
 			if (foundOperationMsg != null) {
-				foundOperationMsg = update(operationMessage,
-						createTransaction);
+				foundOperationMsg = update(operationMessage, createTransaction);
 			} else {
-				foundOperationMsg = create(operationMessage,
-						createTransaction);
+				foundOperationMsg = create(operationMessage, createTransaction);
 			}
 			return foundOperationMsg;
 		}
